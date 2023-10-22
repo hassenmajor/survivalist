9bc484c5b8d734cf1345893678a7a672b707fd68
Hassen Major <hassenmajor@gmail.com>
2023-10-22 11:38:26 +0000

    first commit

--- server.js
+++ server.js
@@ -2,12 +2,27 @@
     Starting point for the app
 */
 
-import { createServer } from 'http';
-import app from './app.js';
+import * as html from 'http'
+import { fs } from 'fs'
 
 const port = process.env.PORT || 3000;
-const server = createServer(app);
+const server = html.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+});
 
-server.listen(port, () => {
-    console.log(`Started on port ${port}`);
+server.listen(port, (error) => {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Server is listening on port ' + port);
+    }
 });
