5de4e0f02980d40e3d4c6035211566d96aaa8296
Hassen Major <hassenmajor@gmail.com>
2023-10-22 10:41:06 +0000

    first commit

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
     Starting point for the app
 */
 
-const http = require('http');
-const app = require('./app');
+import { createServer } from 'http';
+import app from './app';
 
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+const server = createServer(app);
 
 server.listen(port, () => {
     console.log(`Started on port ${port}`);
